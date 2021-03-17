3406eb102c773be1ef400925c9cfeaf3ce617d68
matheusrcruz <git.dev@outlook.com>
2021-03-17 13:52:51 -0300

    ADD: estrutura do spring adicionada

---
+++ bootcamp/src/test/java/com/generation/bootcamp/BootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
