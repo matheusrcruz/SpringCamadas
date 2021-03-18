d576a3a05e7365c70de3ee6d8d4b899829e9b5f9
matheusrcruz <git.dev@outlook.com>
2021-03-18 02:20:18 -0300

    ADD: Repository Camada de Comunicação com banco

---
+++ bootcamp/src/main/java/com/generation/bootcamp/repository/PostagemRepository.java
@@ -0,0 +1,15 @@
+package com.generation.bootcamp.repository;
+
+import com.generation.bootcamp.model.Postagem;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository //Camada de repository Comunicação direta com banco
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+	public List <Postagem> findAllByTituloContainingIgnoreCase(String titulo); /*Busca tudo que pelo estiver mo atributo titulo, tudo que estiver contido e ignora case sensitive */
+
+}
