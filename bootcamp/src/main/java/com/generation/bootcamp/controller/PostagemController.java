c2486ad7d528a1f9e96c88c2ee221685aad3f3a0
matheusrcruz <git.dev@outlook.com>
2021-03-19 01:00:35 -0300

    ADD: Metodo Delete inserido no controller

--- bootcamp/src/main/java/com/generation/bootcamp/controller/PostagemController.java
+++ bootcamp/src/main/java/com/generation/bootcamp/controller/PostagemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -63,6 +64,12 @@ public class PostagemController {
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
 		
 	}
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+		
+	}
+
 	
 	
 	
